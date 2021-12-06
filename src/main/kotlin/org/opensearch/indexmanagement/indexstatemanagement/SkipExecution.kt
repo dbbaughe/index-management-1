4fef8f6d793899180c8b08d184c2c6763e68d593
Drew Baugher <46505179+dbbaughe@users.noreply.github.com>
2021-12-06 14:04:29 -0800

    wip

--- src/main/kotlin/org/opensearch/indexmanagement/indexstatemanagement/SkipExecution.kt
+++ src/main/kotlin/org/opensearch/indexmanagement/indexstatemanagement/SkipExecution.kt
@@ -15,14 +15,20 @@ import org.opensearch.client.Client
 import org.opensearch.cluster.ClusterChangedEvent
 import org.opensearch.cluster.ClusterStateListener
 import org.opensearch.cluster.service.ClusterService
+import org.opensearch.common.settings.Settings
+import org.opensearch.indexmanagement.settings.IndexManagementSettings
 import org.opensearch.indexmanagement.util.OpenForTesting
+import org.opensearch.threadpool.Scheduler
+import org.opensearch.threadpool.ThreadPool
 
 // TODO this can be moved to job scheduler, so that all extended plugin
 //  can avoid running jobs in an upgrading cluster
 @OpenForTesting
 class SkipExecution(
+    settings: Settings,
     private val client: Client,
-    private val clusterService: ClusterService
+    private val clusterService: ClusterService,
+    private val threadPool: ThreadPool
 ) : ClusterStateListener {
     private val logger = LogManager.getLogger(javaClass)
 
@@ -31,18 +37,45 @@ class SkipExecution(
     // To track if there are any legacy IM plugin nodes part of the cluster
     @Volatile final var hasLegacyPlugin: Boolean = false
         private set
+    @Volatile private var skipExecutionCheckPeriod = IndexManagementSettings.SKIP_EXECUTION_CHECK_PERIOD.get(settings)
+    private var scheduledCheck: Scheduler.Cancellable? = null
 
     init {
         clusterService.addListener(this)
+
+        scheduledCheck = threadPool.scheduleWithFixedDelay(
+            { scheduledSweep() },
+            skipExecutionCheckPeriod, ThreadPool.Names.MANAGEMENT
+        )
     }
 
     override fun clusterChanged(event: ClusterChangedEvent) {
+        // 1.0 B/G w/ 1.0 -> should stay false
+        // 1.0 B/G w/ 1.1 -> should go true during B/G and then false once set back
+        // true should always be a temporary thing, as we skip executions during it and that cannot be healthy long term
+        // so we should introduce a periodic sweep of plugin versions in case the flag hasn't set back correctly
+        // if 100 node domain.. don't want them constantly doing node info requests every x minutes
+        // so we will only do it if the flag is set to true to see if the upgrade is done, if flag is false then nothing to check
+        // is this true? if flag is false can we skip? no... because what if that is the check that would
+        // move us to the new flagged state?
         if (event.nodesChanged() || event.isNewCluster) {
             sweepISMPluginVersion()
         }
     }
 
+    fun scheduledSweep() {
+        if (flag) {
+            // can this start running... is getting back false and before it has a chance to update
+            // an event comes in with new nodes that runs and gets back true
+            // and then true is set and then overriden back to false from first?
+            sweepISMPluginVersion()
+        } else {
+            logger.debug("Skipping scheduled sweep as the skip execution flag is already set to false")
+        }
+    }
+
     fun sweepISMPluginVersion() {
+        logger.info("Sweeping ISM plugin versions")
         // if old version ISM plugin exists (2 versions ISM in one cluster), set skip flag to true
         val request = NodesInfoRequest().clear().addMetric("plugins")
         client.execute(
--- src/main/kotlin/org/opensearch/indexmanagement/settings/IndexManagementSettings.kt
+++ src/main/kotlin/org/opensearch/indexmanagement/settings/IndexManagementSettings.kt
@@ -5,6 +5,7 @@
 package org.opensearch.indexmanagement.settings
 
 import org.opensearch.common.settings.Setting
+import org.opensearch.common.unit.TimeValue
 
 class IndexManagementSettings {
 
@@ -13,6 +14,13 @@ class IndexManagementSettings {
         val FILTER_BY_BACKEND_ROLES: Setting<Boolean> = Setting.boolSetting(
             "plugins.index_management.filter_by_backend_roles",
             false,
+            Setting.Property.NodeScope,
+            Setting.Property.Dynamic
+        )
+
+        val SKIP_EXECUTION_CHECK_PERIOD: Setting<TimeValue> = Setting.positiveTimeSetting(
+            "plugins.index_management.skip_execution_check_period",
+            TimeValue.timeValueMinutes(10),
             Setting.Property.NodeScope,
             Setting.Property.Dynamic
         )
